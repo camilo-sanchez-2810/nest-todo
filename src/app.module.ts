b696f0ec72d7a46a4609570e67b8f788d526145e
Camilo <camsanbar@gmail.com>
2023-07-25 12:26:48 -0400

    add: add todo service, module & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, TodosModule],
   controllers: [],
   providers: [],
 })
