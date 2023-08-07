b610e21495c6c0cf5a90cbd91dcc7bbd95d8f4a9
Camilo <camsanbar@gmail.com>
2023-08-07 10:47:03 -0400

    add: add @nest/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodosModule } from './todos/todos.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
     UserModule,
     TodosModule,
     PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
   providers: [],
